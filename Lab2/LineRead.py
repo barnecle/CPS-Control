8179120b713ac1e2780e2703563dc43c2c78df2f
barnecle <barnecle@iu.edu>
2021-09-24 18:41:38 +0100

    LineCurses.py update

--- Lab2/LineRead.py
+++ Lab2/LineRead.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+
 # -*- coding: utf-8 -*-
 """
 Created on Fri Sep 10 15:50:43 2021
@@ -7,11 +7,43 @@ Created on Fri Sep 10 15:50:43 2021
 """
 import curses
 import RPi.GPIO as GPIO
-from AlphaBot2 import AlphaBot2
-from TRSensors import TRSensor
+from python.AlphaBot2 import AlphaBot2
+from python.TRSensors import TRSensor
 import time
 
 import random
+
+class botPosition:
+    def __init__(self):
+        self.Ab = AlphaBot2()
+        self.TR = TRSensor()
+        self.Ab.stop()
+        time.sleep(0.5)
+        for i in range(0,100):
+            if(i<25 or i>= 75):
+                self.Ab.right()
+                self.Ab.setPWMA(30)
+                self.Ab.setPWMB(30)
+            else:
+                self.Ab.left()
+                self.Ab.setPWMA(30)
+                self.Ab.setPWMB(30)
+        self.TR.calibrate()
+        self.Ab.stop()
+
+        self.TR.calibrate()
+        self.position, self.sensors = self.TR.readLine()
+
+    def sensorValues(self):
+        #self.sensors = self.TR.readCalibrated()
+        self.sensors = self.TR.AnalogRead()
+        return self.sensors
+
+
+    def getPosition(self):
+        self.position, self.sensors = self.TR.readLine()
+        return (self.position-2000)
+
 
 def my_raw_input(stdscr, r, c, prompt_string): #I grabbed this function from stack overflow https://stackoverflow.com/questions/21784625/how-to-input-a-word-in-ncurses-screen
     curses.echo() 
@@ -30,26 +62,32 @@ def overscoreString(string):
          retString += i
    return retString
 
-def getRobotPosition(pos):
+def getRobotPosition(pos, lastString):
    retString = ""
    div = 10
    incr = 4000/div
    for i in range(0, (div-1)):
-      if (-2000+(incr*i))<pos and pos<(-2000+(incr*(i+1)):
+      if (2000-(incr*i))>pos and pos>(2000-(incr*(i+1))):
          retString += "O"
       else:
          retString += "_"
-   return retString
+   if "O" in retString:
+      return retString
+   else:
+      return lastString
+
 
 def main(stdscr):
+
     curses.noecho()
     curses.cbreak()
     curses.curs_set(0)
-    stdscr.keypad(True)
+   # stdscr.keypad(True)
     
-    #values = [10,150,70,90,110]
-    freq = 5
-	bot = botPosition()
+    values = [0,0,0,0,0]
+    freq = 1
+    bot = botPosition()
+    posString = " "
 
     while True:
 
@@ -67,14 +105,19 @@ def main(stdscr):
 
         #for i in range(len(values)):
         #   values[i] += random.randint(0,10)
-		values[i] = bot.sensorValues()
-		pos = bot.position()
+        values = bot.sensorValues()
+        pos = bot.getPosition()
 
-        stdscr.addstr(max_y+1,0, "\u203e"*max_x)
+        stdscr.addstr(max_y,0, "_"*max_x)
 
         for i in range(len(values)):
-           height = max_y-int(max_y*(values[i]/max(values)))
-           stdscr.addstr(max_y+1,i*bin_width+1,overscoreString("SENSOR "+str(i+1)))
+           if max(values) == 0:
+              height = max_y-1
+           else:
+              height = max_y-int(max_y*(values[i]/max(values)))
+              if height == max_y: # want space to display value
+                  height = max_y-1
+           stdscr.addstr(max_y+1,i*bin_width+1,"SENSOR "+str(i+1))
            stdscr.addstr(height+1,i*bin_width+1,str(values[i]))
            try:
                for j in range(max_y-height):
@@ -86,46 +129,22 @@ def main(stdscr):
            except:
                stdscr.addstr(0, 0, "oops")
         stdscr.addstr(frequencyLine,0, "Sample Frequency: "+str(freq)+" Hz")
-        stdscr.addstr(positionLine,0, "Robot Relative to line: "+getRobotPosition(pos))
+        posString = getRobotPosition(pos, posString)
+        stdscr.addstr(positionLine,0, "Robot Relative to line: "+posString)
         
-		stdscr.addstr(computedCenterLine,0, "Robot Computed Center Position: ")
-
+        stdscr.addstr(computedCenterLine,0, "Robot Computed Center Position: ")
+        time.sleep((1/freq))
         stdscr.refresh()
 
-        key = stdscr.getch()
+      #  key = stdscr.getch()
 
         # Process the keystroke
-        if key == curses.KEY_RIGHT:
-            continue
-        if key == ord('f'): #typing f promts for new frequency
-           freq = str(int(my_raw_input(stdscr, newFreqLine, 0, "new freq?")))
-        elif key == ord('q'):
-            break
+   #     if key == curses.KEY_RIGHT:
+    #        continue
+       # if key == ord('f'): #typing f promts for new frequency
+        #   freq = str(int(my_raw_input(stdscr, newFreqLine, 0, "new freq?")))
+       # elif key == ord('q'):
+       #     break
 
 curses.wrapper(main)
 
-class botPosition:
-	def __init__(self):
-		self.Ab = AlphaBot2()
-		self.TR = TRSensors()
-		self.TR.calibrate()
-		self.position, self.sensors
-	
-	def sensorValues(self):
-		self.sensors = self.TR.readCalibrated()
-		
-		return self.sensors
-		
-	def onLine(self):
-		self.sensors = self.TR.readCalibrated()
-		lineValue = self.sensors
-		for i in (0, self.sensors.length())
-			if(self.sensors[i]>200):
-				lineValue[i] = 1
-			else:
-				lineValue[i] = 0
-		return lineValue
-		
-	def position(self):
-		self.position, self.sensors = (self.TR.readLine() - 2000)
-		return self.position
