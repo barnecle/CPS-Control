26f7f05d20cdda96a8a789792089df733c72cb19
mfmitsch <mfmitsch@iu.edu>
2021-10-07 23:36:55 +0100

    Update

--- Lab3/Line_Follow.py
+++ Lab3/Line_Follow.py
@@ -19,7 +19,7 @@ GPIO.setup(Button,GPIO.IN,GPIO.PUD_UP)
 #LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
 #LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)	
 
-maximum = 50;
+maximum = 75;
 j = 0
 integral = 0;
 last_proportional = 0
@@ -29,14 +29,14 @@ timeStamp = 0
 
 def Wheel(pos):
 #	"""Generate rainbow colors across 0-255 positions."""
-	if pos < 85:
-		return Color(pos * 3, 255 - pos * 3, 0)
-	elif pos < 170:
-		pos -= 85
-		return Color(255 - pos * 3, 0, pos * 3)
-	else:
-		pos -= 170
-		return Color(0, pos * 3, 255 - pos * 3)
+    if pos < 85:
+        return Color(pos * 3, 255 - pos * 3, 0)
+    elif pos < 170:
+        pos -= 85
+        return Color(255 - pos * 3, 0, pos * 3)
+    else:
+        pos -= 170
+        return Color(0, pos * 3, 255 - pos * 3)
 
 # Create NeoPixel object with appropriate configuration.
 #strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS)
@@ -54,67 +54,68 @@ Ab.stop()
 print("Line follow Example")
 time.sleep(0.5)
 for i in range(0,100):
-	if(i<25 or i>= 75):
-		Ab.right()
-		Ab.setPWMA(30)
-		Ab.setPWMB(30)
-	else:
-		Ab.left()
-		Ab.setPWMA(30)
-		Ab.setPWMB(30)
-	TR.calibrate()
+    if(i<25 or i>= 75):
+        Ab.right()
+        Ab.setPWMA(30)
+        Ab.setPWMB(30)
+    else:
+        Ab.left()
+        Ab.setPWMA(30)
+        Ab.setPWMB(30)
+    TR.calibrate()
 Ab.stop()
 print(TR.calibratedMin)
 print(TR.calibratedMax)
 while (GPIO.input(Button) != 0):
-	position,Sensors = TR.readLine()
-	print(position,Sensors)
-	time.sleep(0.05)
+    position,Sensors = TR.readLine()
+    print(position,Sensors)
+    time.sleep(0.05)
 Ab.forward()
 
 while True:
-	position,Sensors = TR.readLine()
-	print(position-2000)
-	if(Sensors[0] >900 and Sensors[1] >900 and Sensors[2] >900 and Sensors[3] >900 and Sensors[4] >900):
-		Ab.setPWMA(0)
-		Ab.setPWMB(0)
-		pwm_L = 0
-		pwm_R = 0
-	else:
+    position,Sensors = TR.readLine()
+    print(position-2000)
+    if(Sensors[0] >900 and Sensors[1] >900 and Sensors[2] >900 and Sensors[3] >900 and Sensors[4] >900):
+        Ab.setPWMA(0)
+        Ab.setPWMB(0)
+        pwm_L = 0
+        pwm_R = 0
+        break;
+    else:
 		# The "proportional" term should be 0 when we are on the line.
-		proportional = position - 2000
+        proportional = position - 2000
 		
 		# Compute the derivative (change) and integral (sum) of the position.
-		derivative = proportional - last_proportional
-		integral += proportional
+        derivative = proportional - last_proportional
+        integral += proportional
 		
 		# Remember the last position.
-		last_proportional = proportional
+        last_proportional = proportional
 
-		'''
-		// Compute the difference between the two motor power settings,
-		// m1 - m2.  If this is a positive number the robot will turn
-		// to the right.  If it is a negative number, the robot will
-		// turn to the left, and the magnitude of the number determines
-		// the shar + power_differencepness of the turn.  You can adjust the constants by which
-		// the proportional, integral, and derivative terms are multiplied to
-		// improve performance.
-		'''
-		power_difference = proportional/30  + integral/8000+ derivative*3;  
+        '''
+        // Compute the difference between the two motor power settings,
+        // m1 - m2.  If this is a positive number the robot will turn
+        // to the right.  If it is a negative number, the robot will
+        // turn to the left, and the magnitude of the number determines
+        // the shar + power_differencepness of the turn.  You can adjust the constants by which
+        // the proportional, integral, and derivative terms are multiplied to
+        // improve performance.
+        '''
+        power_difference = proportional/35 + derivative*3 +integral/7000;
 
-		if (power_difference > maximum):
-			power_difference = maximum
-		if (power_difference < - maximum):
-			power_difference = - maximum
-		print(position,power_difference)
-		if (power_difference < 0):
-			Ab.setPWMA(maximum + power_difference)
-			Ab.setPWMB(maximum);
+        if (power_difference > maximum):
+            power_difference = maximum
+        if (power_difference < - maximum):
+            power_difference = - maximum
+        print(position,power_difference)
+        if (power_difference < 0):
+            Ab.setPWMA(maximum + power_difference)
+            Ab.setPWMB(maximum);
            # pwm_L = maximum + power_difference
            # pwm_R = maximum
-		else:
-			Ab.setPWMA(maximum);
-			Ab.setPWMB(maximum - power_difference)
+        else:
+            Ab.setPWMA(maximum);
+            Ab.setPWMB(maximum - power_difference)
 		   # pwm_L = maximum
            # pwm_R = maximum + power_difference
         
